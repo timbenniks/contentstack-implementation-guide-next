1f030cc2c36c72f54170d23a758c5b6bc8047886
Tim Benniks <tbenniks@gmail.com>
2024-09-18 17:35:09 +0200

    initial commit

---
+++ lib/contentstack.ts
@@ -0,0 +1,56 @@
+import contentstack, { Region, QueryOperation } from "@contentstack/delivery-sdk"
+import ContentstackLivePreview from "@contentstack/live-preview-utils";
+import { Page } from "./types";
+
+export const stack = contentstack.stack({
+  apiKey: process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY as string,
+  deliveryToken: process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN as string,
+  environment: process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT as string,
+  region: process.env.NEXT_PUBLIC_CONTENTSTACK_REGION === 'EU' ? Region.EU : Region.US,
+  live_preview: {
+    enable: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true',
+    preview_token: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW_TOKEN,
+    host: process.env.NEXT_PUBLIC_CONTENTSTACK_REGION === 'EU' ? "eu-rest-preview.contentstack.com" : "rest-preview.contentstack.com",
+  }
+});
+
+ContentstackLivePreview.init({
+  ssr: false,
+  enable: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true',
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  stackSdk: stack.config,
+  debug: true,
+  stackDetails: {
+    apiKey: process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY as string,
+    environment: process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT as string,
+  },
+  clientUrlParams: {
+    host:
+      process.env.NEXT_PUBLIC_CONTENTSTACK_REGION === "EU"
+        ? "eu-app.contentstack.com"
+        : "app.contentstack.com",
+  },
+  editButton: {
+    enable: true,
+  },
+});
+
+export async function getPage(url: string) {
+  const result = await stack
+    .contentType("page")
+    .entry()
+    .query()
+    .where("url", QueryOperation.EQUALS, url)
+    .find<Page>();
+
+  if (result.entries) {
+    const entry = result.entries[0]
+
+    if (process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true') {
+      contentstack.Utils.addEditableTags(entry, 'page', true);
+    }
+
+    return entry;
+  }
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "eu-images.contentstack.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
